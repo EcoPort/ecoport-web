4bb80d48f6dde84376c581ce7b874ee2f8100be6
EcoPort <ayrtonmudgway4@gmail.com>
2025-06-26 13:24:21 +1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Welcome to EcoPort</h1>
+      <p>EcoPort is a global plastic export business based in Bali, focused on recycling HDPE plastic and shipping to buyers worldwide.</p>
+    </main>
+  );
+}
